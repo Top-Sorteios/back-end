102e0ca7038480620bb7d67d0c58eecbb1a15c93
AnaBia0101 <silvab699@gmail.com>
2024-05-28 17:12:13 -0300

    Add files via upload

---
+++ src/main/java/br/com/topsorteio/repositories/IPremioRepository.java
@@ -0,0 +1,16 @@
+package br.com.topsorteio.repositories;
+
+import br.com.topsorteio.entities.PremioModel;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPremioRepository extends JpaRepository<PremioModel, Integer> {
+	
+	Optional<PremioModel> findByNome(String nome); 
+	
+}
