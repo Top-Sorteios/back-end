49d65ae72880b57a84501c64f05265af840b6775
AnaBia0101 <silvab699@gmail.com>
2024-05-28 17:13:38 -0300

    Update PremioServise.java

--- src/main/java/br/com/topsorteio/service/PremioServise.java
+++ src/main/java/br/com/topsorteio/service/PremioServise.java
@@ -1,5 +1,16 @@
 package br.com.topsorteio.service;
 
+import java.util.List;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.stereotype.Service;
+import br.com.topsorteio.entities.MarcaModel;
+import br.com.topsorteio.exceptions.EventInternalServerErrorException;
+import br.com.topsorteio.repositories.IMarcaRepository;
+
+@Service
 public class PremioServise {
 
 }
