2fda5e745295ca30ca52ee68aaf30bb27ad61191
Marcella Carneiro <carneiromarcella53@gmail.com>
2024-05-31 23:02:52 -0300

    Adicionando métodos get, post, put e delete

--- src/main/java/br/com/topsorteio/controllers/MarcaController.java
+++ src/main/java/br/com/topsorteio/controllers/MarcaController.java
@@ -1,18 +1,21 @@
 package br.com.topsorteio.controllers;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.topsorteio.dtos.ErrorDTO;
+import br.com.topsorteio.dtos.GetAllMarcasResponseDTO;
+import br.com.topsorteio.dtos.MarcaEditRequestDTO;
 import br.com.topsorteio.dtos.MarcaRegisterRequestDTO;
-import br.com.topsorteio.entities.MarcaModel;
 import br.com.topsorteio.service.MarcaService;
 
 @RestController
@@ -20,15 +23,31 @@ import br.com.topsorteio.service.MarcaService;
 public class MarcaController {
 	
 	@Autowired
-	private MarcaService marcaService;
+	private MarcaService repository;
+	
+	@GetMapping
+	@RequestMapping("/obter")
+	public ResponseEntity<List<GetAllMarcasResponseDTO>> obterTodasAsMarcas(){
+		return repository.obterTodasAsMarcas();
+	}
+	
+	@GetMapping
+	@RequestMapping("/obter/{id}")
+	public ResponseEntity<?> obterMarcaPorId(@PathVariable Integer id){
+		return repository.obterMarcaPorId(id);
+	}
 	
 	@PostMapping
-	@RequestMapping("/inserir")
-	public ResponseEntity<?> inserirMarca(@RequestBody MarcaRegisterRequestDTO request){
-		Optional<MarcaModel> marca = this.marcaService.findByName(request.nome());
-		if(marca.isPresent())
-            return ResponseEntity.status(HttpStatus.OK).body(new ErrorDTO(HttpStatus.BAD_REQUEST, 400, "Marca já existe.", false));
-		return ResponseEntity.status(201).body(marcaService.inserirMarca(new MarcaModel(request)));
+	@RequestMapping("/registrar")
+	public ResponseEntity<?> registrarMarca(@RequestBody MarcaRegisterRequestDTO request){
+		return repository.registrarMarca(request);
 	}
-
+	
+	@PutMapping
+	@RequestMapping("/editar/{id}")
+	public ResponseEntity<?> editarMarca(@PathVariable Integer id, @RequestBody MarcaEditRequestDTO request){
+		return repository.editarMarca(id, request);
+	}
+	
+	
 }
