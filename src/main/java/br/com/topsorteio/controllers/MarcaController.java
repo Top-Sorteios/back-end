3d24920534f068ff83331d0d1d113f9da6973698
Guilherme Cavalcante <guicavalcante59@gmail.com>
2024-06-03 17:01:20 -0300

    Adicionando: anotação @CrossOrigin nos controllers

--- src/main/java/br/com/topsorteio/controllers/MarcaController.java
+++ src/main/java/br/com/topsorteio/controllers/MarcaController.java
@@ -5,10 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.topsorteio.dtos.ErrorDTO;
 import br.com.topsorteio.dtos.MarcaRegisterRequestDTO;
@@ -16,6 +13,7 @@ import br.com.topsorteio.entities.MarcaModel;
 import br.com.topsorteio.service.MarcaService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/marcas")
 public class MarcaController {
 	
