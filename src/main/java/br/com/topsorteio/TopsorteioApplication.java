6faf07ce16416c7ae6ad9429d58c202096f9c69c
Guilherme Cavalcante <guicavalcante59@gmail.com>
2024-06-03 20:59:23 -0300

    Removendo a liberação do spring security

--- src/main/java/br/com/topsorteio/TopsorteioApplication.java
+++ src/main/java/br/com/topsorteio/TopsorteioApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class TopsorteioApplication {
 
 	public static void main(String[] args) {
