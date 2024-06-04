93c583a5e353c39b95b32702d4dc379caf8b1f93
Guilherme Cavalcante <guicavalcante59@gmail.com>
2024-06-04 19:54:37 -0300

    Atualizado: Resposta para Usuario

--- src/main/java/br/com/topsorteio/dtos/GetAllUserResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.topsorteio.dtos;
-
-
-import br.com.topsorteio.entities.UserRole;
-
-public record GetAllUserResponseDTO(String nome, String email, UserRole administrador, String status, String datanascimento, String turma) {
-}
