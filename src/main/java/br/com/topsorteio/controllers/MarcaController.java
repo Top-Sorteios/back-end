9c38149d1e8eff47a1939fa08ab4109015373359
Marcella Carneiro <carneiromarcella53@gmail.com>
2024-06-06 18:15:58 -0300

    Update MarcaController

--- src/main/java/br/com/topsorteio/controllers/MarcaController.java
+++ src/main/java/br/com/topsorteio/controllers/MarcaController.java
@@ -4,19 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
->>>>>>> main
 
 import br.com.topsorteio.dtos.GetAllMarcasResponseDTO;
 import br.com.topsorteio.dtos.MarcaEditRequestDTO;
@@ -24,11 +12,7 @@ import br.com.topsorteio.dtos.MarcaRegisterRequestDTO;
 import br.com.topsorteio.service.MarcaService;
 
 @RestController
-<<<<<<< HEAD
-@CrossOrigin(origins = "http://localhost:3000")
-=======
 @CrossOrigin("*")
->>>>>>> main
 @RequestMapping("/marcas")
 public class MarcaController {
 	
@@ -59,8 +43,4 @@ public class MarcaController {
 	public ResponseEntity<?> removerMarca(@PathVariable Integer id){
 		return repository.removerMarca(id);
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> main
