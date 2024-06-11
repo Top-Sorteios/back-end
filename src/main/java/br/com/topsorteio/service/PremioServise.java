c5669b43bf60f769d55f41f8e3daf4b89e4b5de7
AnaBia0101 <silvab699@gmail.com>
2024-06-11 20:54:37 -0300

    Update PremioServise.java

--- src/main/java/br/com/topsorteio/service/PremioServise.java
+++ src/main/java/br/com/topsorteio/service/PremioServise.java
@@ -1,6 +1,18 @@
 package br.com.topsorteio.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.jpa.JpaSystemException;
+import org.springframework.stereotype.Service;
+
 import br.com.topsorteio.dtos.ErrorDTO;
+import br.com.topsorteio.dtos.GetAllPremiosResponseDTO;
 import br.com.topsorteio.dtos.PremioEditRequestDTO;
 import br.com.topsorteio.dtos.PremioRegisterRequestDTO;
 import br.com.topsorteio.entities.MarcaModel;
@@ -8,189 +20,53 @@ import br.com.topsorteio.entities.PremioModel;
 import br.com.topsorteio.exceptions.EventInternalServerErrorException;
 import br.com.topsorteio.repositories.IMarcaRepository;
 import br.com.topsorteio.repositories.IPremioRepository;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.orm.jpa.JpaSystemException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PremioService {
 
     @Autowired
-    private IPremioRepository premioRepository;
+    private IPremioRepository repository;
 
     @Autowired
     private IMarcaRepository marcaRepository;
 
-    
+
 
     public ResponseEntity<Optional<PremioModel>> buscarPorCodigoSku(String codigoSku) {
         try {
-            Optional<PremioModel> premio = premioRepository.findByCodigoSku(codigoSku);
-            // Verifica se o prêmio foi encontrado
-            if (premio.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND); // Retorna 404 se o prêmio não for encontrado
-            }
-            return new ResponseEntity<>(premio, HttpStatus.OK); // Retorna o prêmio encontrado com status 200
-        } catch (JpaSystemException ex) {
-            throw new EventInternalServerErrorException("Erro ao buscar prêmio por SKU"); // Trata exceções do banco de dados
-        }
-    }
-
-    public ResponseEntity<Object> registrarPremio(PremioRegisterRequestDTO data) {
-        // Verifica se já existe um prêmio com o mesmo SKU
-        Optional<PremioModel> premioResponse = premioRepository.findByCodigoSku(data.getCodigoSku());
-
-        // Se o prêmio já existe, retorna um erro de conflito
-        if (premioResponse.isPresent()) {
-            return new ResponseEntity<>(new ErrorDTO(HttpStatus.CONFLICT, 400, "Prêmio já existe.", false), HttpStatus.CONFLICT);
-        }
-
-        try {
-            // Recupera a marca associada do banco de dados
-            MarcaModel marcaModel = marcaRepository.findByNome(data.getMarca())
-                    .orElseThrow(() -> new EventInternalServerErrorException("Marca parceira não encontrada."));
-
-            // Cria uma nova entidade de prêmio usando os dados do DTO
-            PremioModel premioModel = new PremioModel(
-                    data.getCodigoSku(),
-                    data.getNome(),
-                    data.getQuantidade(),
-                    data.getDescricao(),
-                    data.getImagem(),
-                    marcaModel // Associa o prêmio à marca
-            );
-
-            // Salva a nova entidade de prêmio no banco de dados
-            premioModel = premioRepository.save(premioModel);
-
-            // Retorna o prêmio criado com status 201
-            return new ResponseEntity<>(premioModel, HttpStatus.CREATED);
-        } catch (JpaSystemException ex) {
-            // Trata exceções do banco de dados
-            throw new EventInternalServerErrorException("Erro ao salvar prêmio");
-        }
-    }
-    
-    
-    public ResponseEntity<HttpStatus> editarNome(PremioEditRequestDTO request, String codigoSku) {
-        try {
-            Optional<PremioModel> premioOpt = premioRepository.findByCodigoSku(codigoSku);
-
-            if (premioOpt.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-            PremioModel premio = premioOpt.get();
-            premio.setNome(request.nome());
-
-            premioRepository.save(premio);
-
-            return new ResponseEntity<>(HttpStatus.OK);
+            Optional<PremioModel> premio = repository.findByCodigoSku(codigoSku);
+            return ResponseEntity.ok(premio); // Retorna um ResponseEntity com o resultado da busca
         } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
-        }
-    }
-    
-    public ResponseEntity<HttpStatus> editarDescricao(String nome, PremioEditRequestDTO request) {
-        try {
-            Optional<PremioModel> premioOpt = premioRepository.findByNome(nome);
-
-            if (premioOpt.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-            PremioModel premio = premioOpt.get();
-            premio.setDescricao(request.descricao());
-
-            premioRepository.save(premio);
-
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
-        }
-    }
-    
-    
-    public ResponseEntity<HttpStatus> editarImagem(String codigoSku, PremioEditRequestDTO request) {
-        try {
-            Optional<PremioModel> premioOpt = premioRepository.findByCodigoSku(codigoSku);
-
-            if (premioOpt.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-            PremioModel premio = premioOpt.get();
-            
-            // Verifica se a nova imagem foi fornecida
-            if (request.imagem()!= null && request.imagem().length > 0) {
-                premio.setImagem(request.imagem());
-            } else {
-                // Retorna erro se a nova imagem estiver ausente
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
-            premioRepository.save(premio);
-
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
-        }
-    }
-    
-    public ResponseEntity<HttpStatus> editarQuantidade(String codigoSku, PremioEditRequestDTO request) {
-        try {
-            Optional<PremioModel> premioOpt = premioRepository.findByCodigoSku(codigoSku);
-
-            if (premioOpt.isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-            PremioModel premio = premioOpt.get();
-            
-            // Verifica se a nova quantidade é válida (maior que zero)
-            if (request.quantidade() > 0) {
-                premio.setQuantidade(request.quantidade());
-            } else {
-                // Retorna erro se a nova quantidade for inválida
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-            }
-
-            premioRepository.save(premio);
-
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
-        }
-    }
-    
-    public ResponseEntity<HttpStatus> removerPremio(String codigoSku) {
-        try {
-            Optional<PremioModel> premioOpt = premioRepository.findByCodigoSku(codigoSku);
-            if (premioOpt.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-            premioRepository.delete(premioOpt.get());
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
-        }
-    }
-    
-    public ResponseEntity<List<PremioModel>> listarTodosPremios() {
-        try {
-            List<PremioModel> premios = premioRepository.findAll();
-            if (premios.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(premios, HttpStatus.OK);
-        } catch (RuntimeException ex) {
-            throw new EventInternalServerErrorException(ex.getMessage());
+            throw new EventInternalServerErrorException(ex.getMessage()); // Propaga a exceção para ser tratada em outro lugar
         }
     }
 
+    public ResponseEntity<List<GetAllPremiosResponseDTO>> obterTodosOsPremios() {
+        List<PremioModel> premios = repository.findAll();
+        List<GetAllPremiosResponseDTO> response = new ArrayList<>();
+
+        if (premios.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        for (PremioModel premio : premios) {
+            GetAllPremiosResponseDTO premioDTO = new GetAllPremiosResponseDTO(
+                    premio.getCodigoSku(),
+                    premio.getNome(),
+                    premio.getQuantidade(),
+                    premio.getDescricao(),
+                    premio.getImagem(),
+                    premio.getMarca());
+            response.add(premioDTO);
+        } return new ResponseEntity<>(response, HttpStatus.OK);
+    }
     // Método para pesquisar prêmios pelo nome
     public ResponseEntity<List<PremioModel>> pesquisarPremiosPorNome(String nome) {
         try {
-            List<PremioModel> premios = premioRepository.findByNomeContainingIgnoreCase(nome);
+            List<PremioModel> premios = repository.findByNomeContainingIgnoreCase(nome);
             if (premios.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -199,8 +75,63 @@ public class PremioService {
             throw new EventInternalServerErrorException(ex.getMessage());
         }
     }
-    
-    
-    
+    public ResponseEntity<?> registrarPremio(PremioRegisterRequestDTO request) {
+        try {
+
+            MarcaModel marcaModel = marcaRepository.findByNome(request.marca().getNome())
+                    .orElseGet(() -> {
+                        MarcaModel newMarca = new MarcaModel();
+                        newMarca.setNome(request.marca().getNome());
+                        return marcaRepository.save(newMarca);
+                    });
+
+            Optional<PremioModel> premioExistente = repository.findByCodigoSku(request.codigoSku());
+            if (premioExistente.isPresent()) {
+                return ResponseEntity.status(HttpStatus.CONFLICT)
+                        .body(new ErrorDTO(HttpStatus.CONFLICT, 409, "Prêmio já existe.", false));
+            }
+
+            PremioModel premioModel = new PremioModel(request.codigoSku(), request.nome(), request.quantidade(), request.descricao(),
+                    request.imagem(), marcaModel, request.criadoPor());
+            premioModel = repository.save(premioModel);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(premioModel);
+        } catch (Exception e) { throw new RuntimeException(e);}
+    }
+
+    public ResponseEntity<?> editarPremio(@PathVariable String codigoSku, @RequestBody PremioEditRequestDTO request) {
+        try {
+            Optional<PremioModel> premioOpt = repository.findByCodigoSku(codigoSku);
+
+            if (premioOpt.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            PremioModel premio = premioOpt.get();
+
+            premio.setNome(request.nome() != null ? request.nome() : premio.getNome());
+            premio.setDescricao(request.descricao() != null ? request.descricao() : premio.getDescricao());
+            premio.setImagem(request.imagem() != null && request.imagem().length > 0 ? request.imagem() : premio.getImagem());
+            premio.setQuantidade(request.quantidade() > 0 ? request.quantidade() : premio.getQuantidade());
+
+            repository.save(premio);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (RuntimeException ex) {
+            throw new EventInternalServerErrorException(ex.getMessage());
+        }
+    }
+
+    public ResponseEntity<?> removerPremio(@PathVariable String codigoSku) {
+        Optional<PremioModel> premioOpt = repository.findByCodigoSku(codigoSku);
+
+        if (premioOpt.isEmpty()) {
+            return ResponseEntity.notFound().build(); // Retorna um ResponseEntity com status NOT_FOUND se o prêmio não for encontrado
+        }
+
+        repository.delete(premioOpt.get()); // Deleta o prêmio encontrado
+
+        return ResponseEntity.noContent().build(); // Retorna um ResponseEntity com status NO_CONTENT
+    }
 }
-    
+
