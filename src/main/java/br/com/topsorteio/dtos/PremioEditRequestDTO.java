6d177bb53b50568fd543983b3eb4007c71ad6abc
AnaBia0101 <silvab699@gmail.com>
2024-06-11 20:50:29 -0300

    Add files via upload

--- src/main/java/br/com/topsorteio/dtos/PremioEditRequestDTO.java
+++ src/main/java/br/com/topsorteio/dtos/PremioEditRequestDTO.java
@@ -1,4 +1,5 @@
 package br.com.topsorteio.dtos;
 
 public record PremioEditRequestDTO(String nomeAtual, String nome, int quantidadeAtual, int quantidade, 
-								String descricaoAtual, String descricao, byte[] imagemAtual, byte[] imagem) {}
+								String descricaoAtual, String descricao, byte[] imagemAtual, byte[] imagem
+								) {}
--- src/main/java/br/com/topsorteio/dtos/PremioRegisterRequestDTO.java
+++ src/main/java/br/com/topsorteio/dtos/PremioRegisterRequestDTO.java
@@ -2,6 +2,6 @@ package br.com.topsorteio.dtos;
 
 import br.com.topsorteio.entities.MarcaModel;
 
-public record PremioRegisterRequestDTO (String codigoSku, String nome, int quantidade, 
-										String descricao, byte[] imagem, MarcaModel marca) {}
- 
+public record PremioRegisterRequestDTO(String codigoSku, String nome, int quantidade,
+									   String descricao, byte[] imagem, MarcaModel marca,
+									   Integer criadoPor) {}
