78721a9f76a1faea543afb45efa17394d0e9a723
AnaBia0101 <silvab699@gmail.com>
2024-06-11 12:46:25 -0300

    Revert "Ana bia"

--- src/main/java/br/com/topsorteio/dtos/PremioRegisterRequestDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.topsorteio.dtos;
-
-import br.com.topsorteio.entities.MarcaModel;
-
-public record PremioRegisterRequestDTO (String codigoSku, String nome, int quantidade, 
-										String descricao, byte[] imagem, MarcaModel marca) {}
- 
-
