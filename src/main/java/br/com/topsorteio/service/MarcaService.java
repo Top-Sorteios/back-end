c82675a1d246d59e0be0a3bdc278f90980ba30d1
Marcella Carneiro <carneiromarcella53@gmail.com>
2024-06-12 20:37:01 -0300

    Uptade: MarcaService

--- src/main/java/br/com/topsorteio/service/MarcaService.java
+++ src/main/java/br/com/topsorteio/service/MarcaService.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.topsorteio.dtos.ErrorDTO;
 import br.com.topsorteio.dtos.GetAllMarcasResponseDTO;
