5119470c4467a60a0280844c172d240606c777a5
Marcella Carneiro <carneiromarcella53@gmail.com>
2024-06-13 14:10:04 -0300

    Deletado: GetAllPremiosResponseDTO

--- src/main/java/br/com/topsorteio/dtos/GetAllPremiosResponseDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.topsorteio.dtos;
-
-public record GetAllPremiosResponseDTO(String nome, String codigoSku, byte[] imagem, int quantidade, String descricao, Integer marcaid, String marcaNome) {
-}
